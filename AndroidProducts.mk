f2ea9727c657b6d46ca914cb0b5ab5d6eb32ecb8
Dedrimer <13420015738@sina.cn>
2022-04-01 18:17:17 +0800

    Files:twrp device tree files

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_nio.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_nio-user \
+    omni_nio-userdebug \
+    omni_nio-eng
